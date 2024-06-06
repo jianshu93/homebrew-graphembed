2a6d3632dc68d8981e03d297f0784b6a8023a87e
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-06-06 15:01:27 -0400

    Update graphembed to v0.1.2
    Update graphembed to v0.1.2

--- Formula/graphembed.rb
+++ Formula/graphembed.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Graphembed < Formula
-  desc "Efficient Graph Embedding via Recursive Sketching or High-order Transitivity Preserving"
+  desc "Efficient Graph Embedding via High-order Transitivity Preservation or Recursive Sketching"
   homepage "https://github.com/jean-pierreBoth/graphembed"
-  url "https://github.com/jianshu93/graphembed/releases/download/v0.1.1/graphembed_macos_universal_v0.1.1.tar.gz"
-  sha256 "4953427eb5036b4dc3d49133bc38ed11304910041f0d86267fdad9eb38b890b2"
-  version "0.1.1"
+  url "https://github.com/jianshu93/graphembed/releases/download/v0.1.2/graphembed_macos_universal_v0.1.1.tar.gz"
+  sha256 "52101fd22392539e9844a4f0dd4f373ec7852d0298022ba4b336c56cd91bfcc8"
+  version "0.1.2"
 
   depends_on "openblas"
 
