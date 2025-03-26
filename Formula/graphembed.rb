e95d9b404ecaf721b7c1af5955242d616e599d14
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-03-25 19:08:47 -0700

    Update graphembed.rb

--- Formula/graphembed.rb
+++ Formula/graphembed.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Graphembed < Formula
   desc "Efficient Network/Graph Embedding via High-order Transitivity Preservation or Recursive Sketching"
-  homepage "https://github.com/jean-pierreBoth/graphembed"
+  homepage "https://gitlab.com/Jianshu_Zhao/graphembed"
   url "https://github.com/jianshu93/graphembed/releases/download/v0.1.4/graphembed_universal.tar.gz"
-  sha256 "0e3e565bb4fd71100edf05c981d8b544bc87a16ea58b33d61676ddcd388bb0f9"
-  version "0.1.4"
+  sha256 "e7ae024bd51f60e2eb9135947172267bee780f5fe39cd698faeaf15112321056"
+  version "0.1.5"
 
   depends_on "openblas"
 
